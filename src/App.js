fa821ff3a8db09fbe515f38faf17519e1a14dfc8
niravdoshi15 <niravdoshi15@gmail.com>
2018-08-18 00:20:21 +0530

    Some more Refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login'
 import { Provider } from 'react-redux'
@@ -10,13 +9,6 @@ class App extends Component {
     return (
       <Provider store={store}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Login />
       </div>
       </Provider>
